ad97d508e6a4ffe76eca85bd2ce8750579af9350
agrus101 <agrus101@gmail.com>
2025-10-02 07:16:35 +0000

    ininital working version

---
+++ transcriber.py
@@ -0,0 +1,21 @@
+import whisper
+import datetime
+import argparse
+
+parser = argparse.ArgumentParser(description='Transcription using Whisper')
+parser.add_argument('name', metavar='n', type=str,
+                    help='file name')
+parser.add_argument('output', metavar='o', type=str,
+                    help='output file')
+
+args = parser.parse_args()
+
+def transcribe(name):
+    
+    return result 
+
+if __name__ == "__main__":
+    output = transcribe(args.name)
+    with open("./log/"+args.output) as f:
+        f.write(output)
+
